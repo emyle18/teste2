edc87803e99ffcae5d3843bcdcf34045d457a8af
emyle18 <74387585+emyle18@users.noreply.github.com>
2020-12-07 01:11:57 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# teste2
+1- Adicionamos uma página com o documento chamado formulario.html e utilizamos o form para fazer o formulário de cadastro.
+
+Ele contém 7 tipos de input como : url, text, email, date, checkbox, tel, password.
+
+2- Foi aplicado media queries para celular com telas 360x742 e tablets com telas de 800x1280 em algumas páginas, como: duvidas, tabela tricional, dietas, ajuda.
+
+
+Equipe: Luize Keyla da Silva Oliveira 
+        Emyle Vitória Silva da Paz Martins
+
---
+++ js/main.js
@@ -0,0 +1,35 @@
+function mudar(){
+    document.getElementById("celke").src = "img/images/clique1.jpg";
+}
+
+function mudarDuvidas(){
+    document.getElementById("celke").src = "img/images/duvidas2.jpg";
+}
+
+
+function mudarAjuda(){
+    document.getElementById("celke").src = "img/images/covid2.jpg";
+}
+
+
+function muda(msg1){
+    document.getElementById("msg1").style.color="rgb(62, 148, 76)";
+    }
+
+
+
+    function funcao1()
+    {
+    
+    alert("Você irá para o começo da página!");
+    }
+
+    function funcao2()
+    {
+    
+    alert("Cadastro feito com sucesso");
+    }
+
+     
+    
+
